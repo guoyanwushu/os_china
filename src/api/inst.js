b2e9e7ca87553f2ad59b4bdb7ca0f80efa7c037f
gang.yi <gang.yi@tellyes.com>
2019-02-13 11:49:37 +0800

    对网络请求的加载机制和错误处理机制作了一些处理

--- src/api/inst.js
+++ src/api/inst.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
+import { Indicator, Toast } from 'mint-ui'
 const inst = axios.create({
   // baseURL: 'https://www.oschina.net/action/openapi',
   baseURL: '/api',
-  timeout: 10000,
+  timeout: 5000,
+  retry: 4,
+  retryDelay: 1000,
+  retryRequests: {}
 })
 const GET_OPTIONS = {
   access_token: process.env.VUE_APP_TOKEN,
@@ -14,16 +18,49 @@ export function Get(url, data) {
 }
 inst.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
+  Indicator.open('加载中...');
   return config;
 }, function (error) {
   // 对请求错误做些什么
+  console.log("request:"+error);
   return Promise.reject(error);
 });
 inst.interceptors.response.use(function (response) {
   if (response.status == '200' && response.data) {
+    Indicator.close();
     return response.data
   }
-}, function (error) {
+}, function (err) {
   // 对响应错误做点什么
-  return Promise.reject(error);
+  Indicator.close();
+  const config = err.config;
+  if (err.code === 'ECONNABORTED' && err.message.indexOf('timeout')!=-1) {
+    inst.defaults.retryRequests[config.url] = inst.defaults.retryRequests[config.url]||1
+    var retryCount = inst.defaults.retryRequests[config.url];
+    if (retryCount >= config.retry) {
+      delete inst.defaults.retryRequests[config.url]
+      Toast({
+        message: '网络异常，请检查您的网络!',
+        position: 'center',
+        duration: 1000
+      });
+      return Promise.reject(err);
+    } else {
+      Toast({
+        message: '请求超时,正在重连!',
+        position: 'center',
+        duration: 1000
+      });
+      inst.defaults.retryRequests[config.url]++;
+      config.baseURL = ''
+      setTimeout(function () {
+        inst(config)
+      }, config.retryDelay)
+
+    }
+
+  } else {
+    Toast(err.message);
+    return Promise.reject(err);
+  }
 });
